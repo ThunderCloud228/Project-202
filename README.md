42dbedbf75bbe454df4c814b4f77dac4ec1d0552
ThunderCloud228 <83110279+ThunderCloud228@users.noreply.github.com>
2023-01-07 15:44:06 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+AF_INET represents IPv4 while AF_INET6 represents IPv6.
+TCP-SOCK_STREAM ;UDP-SOCK_DGRAM.
---
+++ server.py
@@ -0,0 +1,48 @@
+import socket
+from threading import Thread
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ip_address = "127.0.0.1"
+port = 8000
+
+server.bind((ip_address, port))
+server.listen()
+
+clients = []
+
+print("Server is running")
+
+def clientThread(conn, addr):
+    conn.send("Welcome to this chatroom".encode("utf-8"))
+    while True:
+        try:
+            message = conn.recv(2048).decode('utf-8')
+            if message:
+                print ("<" + addr[0] + "> " + message)
+
+                message_to_send = "<" + addr[0] + "> " + message
+                broadcast(message_to_send, conn)
+            else:
+                remove(conn)
+        except:
+            continue
+
+def broadcast(message, connection):
+    for clients in clients:
+        if clients!=connection:
+            try:
+                clients.send(message.encode('utf-8'))
+            except:
+                remove(clients)
+
+def remove(connection):
+    if connection in clients:
+        clients.remove(connection)
+
+while True:
+    conn, addr = server.accept()
+    clients.append(conn)
+    print (addr[0] + " connected")
+    new_thread = Thread(target=clientThread, args=(conn, addr))
+    new_thread.start()
+
